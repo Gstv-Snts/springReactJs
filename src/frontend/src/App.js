becc9744facf773c6e4ab4b517db327ce779bc97
Gustavo Santos <85800061+Gstv-Snts@users.noreply.github.com>
2022-04-01 12:58:39 -0300

    App.js

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,21 +1,13 @@
-import { useEffect, useState } from "react";
-import axios from 'axios'
+
+import GetTodos from './components/getTodos.js'
+import PostTodos from './components/postTodos.js'
+
 export default function App() {
-  const [todos, settodos] = useState()
-
-  useEffect(() => {
-    const a = async () => await axios.get('http://localhost:8090/api/v1/todo').then((res) => {
-      settodos(res.data[0])
-    })
-    a()
-  }, []
-  )
-
 
   return (
     <div className="App">
-      <p>{todos?.task}</p>
-      <p>{todos?.created}</p>
+      <GetTodos />
+      <PostTodos />
     </div>
   );
 }
